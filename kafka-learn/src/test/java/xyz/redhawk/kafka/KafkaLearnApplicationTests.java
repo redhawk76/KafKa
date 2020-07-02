5451cc735fd84c5df93b4c32615f9a8ae42c508b
redhawk <972419185@qq.com>
2020-07-02 10:32:41 +0800

    test github

---
+++ kafka-learn/src/test/java/xyz/redhawk/kafka/KafkaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.redhawk.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
